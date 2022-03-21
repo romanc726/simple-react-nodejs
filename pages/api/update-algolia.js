303bc1670c95168c3a4dc2fbd705ab41c6b1c1a0
romanc726 <cherksv.roman@gmail.com>
2022-03-20 20:10:13 -0400

    update algolia-search

--- pages/api/update-algolia.js
+++ pages/api/update-algolia.js
@@ -1,20 +1,11 @@
-
-import sanityClient from '@sanity/client'
 import algoliasearch from 'algoliasearch'
-import indexer, { flattenBlocks } from 'sanity-algolia'
  
 const algolia = algoliasearch(
   process.env.ALGOLIA_APPLICATION_ID,
   process.env.ALGOLIA_ADMIN_API_KEY
 )
 
-const sanity = sanityClient({
-  dataset: process.env.SANITY_PROJECT_DATASET,
-  projectId: process.env.SANITY_PROJECT_ID,
-  token: process.env.SANITY_API_TOKEN,
-  apiVersion: '2021-03-25',
-  useCdn: false,
-})
+const algoliaIndex = algolia.initIndex('my-index')
 
 export default async function handler(req, res) {
   if (req.headers['content-type'] !== 'application/json') {
@@ -23,48 +14,19 @@ export default async function handler(req, res) {
     return
   }
 
-  const algoliaIndex = algolia.initIndex('my-index')
+  const { created, updated, deleted } = req.body.ids
 
-  const sanityAlgolia = indexer(
-    {
-      post: {
-        index: algoliaIndex,
-        projection: `{
-          title,
-          "path": slug.current,
-          "body": pt::text(body)
-        }`,
-      },
-      article: {
-        index: algoliaIndex,
-        projection: `{
-          heading,
-          "body": pt::text(body),
-          "authorNames": authors[]->name
-        }`,
-      },
-    },
-    (document) => {
-      switch (document._type) {
-        case 'post':
-          return {
-            title: document.title,
-            path: document.slug.current,
-            body: flattenBlocks(document.body),
-          }
-        case 'article':
-          return {
-            title: document.heading,
-            excerpt: flattenBlocks(document.excerpt),
-            body: flattenBlocks(document.body),
-          }
-        default:
-          throw new Error('You didnt handle a type you declared interest in')
-      }
-    },
-  )
+  created.forEach(id => {
+    if (id !== null) algoliaIndex.saveObject({ objectID: id })
+  })
 
-  await sanityAlgolia.webhookSync(sanity, req.body)
+  updated.forEach(id => {
+    if (id !== null) algoliaIndex.partialUpdateObject({ objectID: id })
+  })
+
+  deleted.forEach(id => {
+    if (id !== null) algoliaIndex.deleteObject({ objectID: id })
+  })
 
   return res.status(200).send('ok')
 }
